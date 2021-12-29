7fbe3aef8f509e505f06f68716fa267b4115746d
TKForgeron <trollololollolol.js@gmail.com>
2021-12-29 14:40:15 +0100

    add comments

--- scraper.py
+++ scraper.py
@@ -6,10 +6,26 @@ from functools import partial
 
 
 class Repository:
-    def __init__(self, repo_link: str, username: str, token: str) -> None:
+    def __init__(
+        self, repo_link: str, lines_of_code: int, username: str, token: str
+    ) -> None:
         self.repo_link = repo_link
         self.username = username
+        self.lines_of_code = lines_of_code
         self.token = token
+
+    def get_releases(self):
+        """
+
+        We only analyze projects that release their code (via Git)
+
+
+        """
+        # /repos/{owner}/{repo}/releases
+        endpoint = f"https://api.github.com/repos/{self.repo_link.split('/')[-2]}/{self.repo_link.split('/')[-1]}/releases?per_page=100&anon=1"
+        response = requests.get(endpoint, auth=(self.username, self.token)).json()
+
+        return response
 
     def get_num_collaborators(self):
 
@@ -39,6 +55,10 @@ class Repository:
 
         Get list of commits from API
         Use scraper to get data
+
+        only scrape repos with 50+ commits, --bc less than that doesn't represent code projects that work together (via Git)
+        random sample 100 commits (when repo has 100+ commits) for locpc --bc we are limited in computational power
+
 
 
         """
@@ -81,3 +101,25 @@ class Repository:
         mean_deleted_locpc = sum(deleted_lines) / len(commit)
 
         return mean_added_locpc - mean_deleted_locpc
+
+
+NHapiTools_repo = Repository(
+    repo_link="https://github.com/dib0/NHapiTools", username="", token=""
+)
+
+# rel = NHapiTools_repo.get_releases()
+# rel0_created_date = rel[0]["created_at"]
+# rel0_published_date = rel[0]["published_at"]
+
+# rel1_created_date = rel[1]["created_at"]
+# rel1_published_date = rel[1]["published_at"]
+
+# print(rel0_published_date)
+# print(rel1_published_date)
+
+coll = NHapiTools_repo.get_num_collaborators()
+for col in coll:
+    # print(col["name"])
+    print(col["contributions"])
+    # print(col.keys())
+    print("-" * 16)
